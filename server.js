3a8155e1fcd4a456ec52b120bc5acdbc082d000e
YWDCHEN <ywdchen@uwaterloo.ca>
2023-08-16 23:52:12 -0400

    changes

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 // server used to send send emails
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3001, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running"));
 // console.log(process.env.EMAIL_USER);
 // console.log(process.env.EMAIL_PASS);
 
